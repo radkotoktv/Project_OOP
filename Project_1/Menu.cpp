f08ebeecbc1f2752f8fb86ea4e73f9bd43c4c592
Radoslav Karatanev <rmkrmkrmk13@gmail.com>
2022-05-11 23:49:04 +0300

    Adds a very basic menu.

---
+++ Project_1/Menu.cpp
@@ -0,0 +1,11 @@
+#include "Menu.h"
+#include <iostream>
+Menu::Menu() {
+	std::cout << "_____________________________" << std::endl;
+	std::cout << "1\t Sorted List" << std::endl;
+	std::cout << "2\t Find a book" << std::endl;
+	std::cout << "3\t Add a book" << std::endl;
+	std::cout << "4\t Remove a book" << std::endl;
+	std::cout << "5\t Cout a book" << std::endl;
+	std::cout << "_____________________________" << std::endl;
+}
--- Project_1/main.cpp
+++ Project_1/main.cpp
@@ -2,10 +2,13 @@
 #include <iostream>
 #include "Book.h"
 #include "Admin.h"
+#include "Library.h"
+#include "Menu.h"
 int main() {
 	//Book kniga("Ivan Vazov", "Pod Igoto", "Texta_na_Pod_Igoto.txt", "Stranno", 5, 1234);
 	//Book _kniga;
 	//Admin user;
 	//user.isAdmin();
 	//std::cout << kniga;
+	Menu menu;
 }
